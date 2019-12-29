818bc0d25f32314c3813d2c72b90b3fe1b774f15
Ron Almog <ron8132@gmail.com>
2019-12-29 14:44:23 -0500

    add google maps

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '@/plugins/vuetify'
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ plugins/vuetify.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDDSghJn0wTvgqwnmkMun3CQYmXAewRRN0',
+    libraries: 'places',
+  },
+})
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#121212',
+    accent: colors.grey.darken3,
+    secondary: colors.amber.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3
+  }
+})
